4e218efb0761414041d6c67476ca91e69a9a15c2
AdarshSinghTomar <veracryptic@gmail.com>
2022-08-30 14:48:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -31,4 +31,34 @@ onload =() =>{
     };
     fileReader.readAsText(uploadedFile,"UTF-8");
   };
+  decode.onclick = function () {
+
+    const uploadedFile = upload.files[0];
+    if(uploadedFile===undefined){
+        alert("No file uploaded !");
+        return;
+    }
+    const fileReader = new FileReader();
+    fileReader.onload = function(fileLoadedEvent){
+        const text = fileLoadedEvent.target.result;
+        if(text.length===0){
+            alert("Text can not be empty ! Upload another file !");
+            return;
+        }
+        let [decoded, tree_structure, info] = coder.decode(text);
+        downloadFile(uploadedFile.name.split('.')[0] +'_decoded.txt', decoded);
+        treearea.innerText = tree_structure;
+        treearea.style.marginTop = '2000px';
+        temptext.innerText = info;
+    };
+    fileReader.readAsText(uploadedFile, "UTF-8");
+};
+
+};
+
+function downloadFile(fileName, data){
+let a = document.createElement('a');
+a.href = "data:application/octet-stream,"+encodeURIComponent(data);
+a.download = fileName;
+a.click();
 }
